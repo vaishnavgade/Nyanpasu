358f1ce8e53650fb0e174467bba25311bc3c7ed7
Sri Vaishnav Gade <vaishnav.gade@gmail.com>
2021-02-03 10:24:54 -0500

    [Setup] Setting up projects for endpoints and unit tests

---
+++ Nyanpasu.Tests/NyanControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Nyanpasu.Tests
+{
+    public class NyanControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Nyanpasu/Controllers/NyanController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nyanpasu.Controllers
+{
+    [ApiController]
+    [Route("api/nyan")]
+    public class NyanController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllNyans()
+        {
+            try
+            {
+                return Ok("All Nyans");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        
+    }
+}
